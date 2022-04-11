6a48b896ce0779f450c4d1e671ef09c9f73582b0
raziel <sachuk.aa@intechs.by>
2022-04-11 11:50:43 +0300

    test read message consumer.py from kafka

--- consumer.py
+++ consumer.py
@@ -1,5 +1,7 @@
 from aiokafka import AIOKafkaConsumer
 import asyncio
+import json
+
 
 async def consume():
     consumer = AIOKafkaConsumer(
@@ -11,9 +13,19 @@ async def consume():
         # Consume messages
         async for msg in consumer:
             print("consumed: ", msg.topic, msg.partition, msg.offset,
-                  msg.key, msg.value, msg.timestamp)
+                  msg.key, json.loads(msg.value), msg.timestamp)
+            message = json.loads(msg.value)
+            print(f"TYPE EVENT !!!!! {message.get('type_event')}")
+            print(f"SEND TYPE !!!!! {message.get('send_type')}")
+            print(f"PARAMETERS !!!!! {message.get('parameters')}")
+            print(f"EMAIL FROM PARAMETERS !!!!! {message['parameters'].get('email')}")
+            print(f"CHAT_ID FROM PARAMETERS !!!!! {message['parameters'].get('CHAT_ID')}")
+            print(f"TEXT FROM PARAMETERS !!!!! {message['parameters'].get('text')}")
+
+
     finally:
         # Will leave consumer group; perform autocommit if enabled.
         await consumer.stop()
+
 
 asyncio.run(consume())
