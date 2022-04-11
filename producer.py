5ed3b2e41c596283dba14475decb17722d065aa9
raziel <sachuk.aa@intechs.by>
2022-04-11 11:50:18 +0300

    test send message from producer.py to kafka

--- producer.py
+++ producer.py
@@ -1,34 +1,46 @@
+import json
+import os
+from asyncio import sleep
+import random
 from aiokafka import AIOKafkaProducer
 import asyncio
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+
+events = ['once a week', 'once a day', 'once a month', 'once a year']
+
+send_type = ['email', 'telegram', 'viber', 'what\'s app', 'odnoklassniki']
 
 
 async def send_one():
     producer = AIOKafkaProducer(
-        bootstrap_servers='localhost:9093')
+        bootstrap_servers='localhost:9093', value_serializer=json_serializer)
     # Get cluster layout and initial topic/partition leadership information
     await producer.start()
     try:
         # Produce message
-        await producer.send_and_wait("test", b"Super message")
+        data = {
+            'type_event': random.choice(events),
+            'send_type': random.choice(send_type),
+            'parameters': {
+                'email': os.getenv('MY_EMAIL'),
+                'CHAT_ID': os.getenv('CHAT_ID'),
+                'text': f'This is test text message {random.randrange(1, 100)} '
+            }
+        }
+        await producer.send_and_wait("test", data)
     finally:
         # Wait for all pending messages to be delivered or expire.
         await producer.stop()
+    await sleep(5)
 
 
-asyncio.run(send_one())
-
-
-# async def send_one():
-#     producer = AIOKafkaProducer(loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
-#     # get cluster layout and initial topic/partition leadership information
-#     await producer.start()
-#     try:
-#         # produce message
-#         msg_id = f'{randint(1, 10000)}'
-#         value = {'message_id': msg_id, 'text': 'some text', 'state': randint(1, 100)}
-#         print(f'Sending message with value: {value}')
-#         value_json = json.dumps(value).encode('utf-8')
-#         await producer.send_and_wait(KAFKA_TOPIC, value_json)
-#     finally:
-#         # wait for all pending messages to be delivered or expire.
-#         await producer.stop()
+while True:
+    asyncio.run(send_one())
