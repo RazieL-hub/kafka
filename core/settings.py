66fa49ea387de6746b81b2df315ecd89bfa5040d
raziel <sachuk.aa@intechs.by>
2022-04-11 18:10:30 +0300

    add settings.py for configuration FASTAPI MAIL, and another settings.py

---
+++ core/settings.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from fastapi_mail import ConnectionConfig
+
+load_dotenv()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_FOLDER_EMAIL = os.path.join(BASE_DIR, 'templates')
+
+# CREDENTIALS for EMAIL
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'),
+    MAIL_PORT=int(os.getenv('MAIL_PORT')),
+    MAIL_SERVER=os.getenv('MAIL_SERVER'),
+    MAIL_FROM=os.getenv('MAIL_FROM'),
+    MAIL_FROM_NAME=os.getenv('MAIL_FROM_NAME'),
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    TEMPLATE_FOLDER=TEMPLATE_FOLDER_EMAIL
+)
+
+# CREDENTIALS for TELEGRAM
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
