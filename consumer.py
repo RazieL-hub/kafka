2ffaabbcd1195d0a7052bce1d197713b109014be
raziel <sachuk.aa@intechs.by>
2022-04-14 14:19:52 +0300

    correct consumer.py

--- consumer.py
+++ consumer.py
@@ -4,18 +4,13 @@ import json
 
 
 async def consume():
-    consumer = AIOKafkaConsumer(
-        'test',
-        bootstrap_servers='localhost:9093')
-    # Get cluster layout and join group `my-group`
+    consumer = AIOKafkaConsumer('test', bootstrap_servers='localhost:9093')
     await consumer.start()
     try:
-        # Consume messages
         async for msg in consumer:
             print("consumed: ", msg.topic, msg.partition, msg.offset,
                   msg.key, json.loads(msg.value), msg.timestamp)
     finally:
-        # Will leave consumer group; perform autocommit if enabled.
         await consumer.stop()
 
 
