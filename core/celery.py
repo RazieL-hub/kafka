d0500e471a183f9db576c7a06dd81a39b688d582
raziel <sachuk.aa@intechs.by>
2022-04-14 14:17:34 +0300

    add update task from DB

--- core/celery.py
+++ core/celery.py
@@ -1,16 +1,16 @@
 from celery import Celery
+from sqlalchemy import text
 
-from db.table import reports
+from db.database_sync import SessionLocal
+from db.querys import update_task_status
 
 celery_app = Celery("worker", backend="rpc://user:bitnami@rabbitmq:5672//",
                     broker="amqp://user:bitnami@rabbitmq:5672//")
 celery_app.conf.update(task_track_started=True)
 
 
-
-
 @celery_app.task(acks_late=True)
-def test_celery(word: str, task_record_id: int) -> str:
-    # TODO update row in sql
-
-    return f"test task return {word}"
+def test_celery(task_id: int):
+    with SessionLocal() as db:
+        db.execute(text(update_task_status).bindparams(task_id=task_id))
+        db.commit()
